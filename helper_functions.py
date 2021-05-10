24f0c5cdb8e648b8ff547d2ba906e325ef521368
corey-brennan <33161545+corey-brennan@users.noreply.github.com>
2021-05-10 17:41:48 -0400

    Initial Project Commit
    Added basic functionality for project.

---
+++ helper_functions.py
@@ -0,0 +1,62 @@
+import requests
+
+def calculate_grade(perc):
+
+    perc = perc * 100 
+    if perc >= 90:
+        return('A')
+    elif perc >= 80:
+        return('B')
+    elif perc >= 70:
+        return('C')
+    elif perc >= 60:
+        return('D')
+    else:
+        return('F')
+
+def calculate_article_health(urls):
+
+    # Setting up variables
+    working = [] # Holds working urls
+    notWorking = [] # Holds non-working urls
+    numWorking = 0 # Holdings number of working urls
+    total = len(urls) # Total number of urls entered
+
+    # Iterating through URLs
+    for url in urls:
+        
+        # Checking to ensure url is appropriate type
+        if type(url) == str:
+            
+            # Ensuring protocol is specified
+            if url[0:7] != "http://" and url[0:8] != "https://":
+                url = "https://" + url
+            
+            # Trying to request the url
+            try:
+                r = requests.get(url)
+            except requests.exceptions.RequestException as e:  # This is the correct syntax
+                notWorking.append(url)
+                break;
+                
+            # Checking if status code is appropriate
+            if r and r.status_code >= 200 and r.status_code < 300:
+                
+                working.append(url)
+                numWorking += 1
+            else:
+                notWorking.append(url)
+        
+        # If unexpected datatype, put into not working
+        else:
+            notWorking.append(url)
+
+    # Creating result obj to return
+    result = {
+        'working' : working,
+        'notWorking' : notWorking,
+        'percentWorking' : numWorking/total,
+        'grade' : calculate_grade(numWorking/total)
+    }
+
+    return result
---
+++ main.py
@@ -0,0 +1,52 @@
+# Author: Corey Brennan
+# ONID: brennaco
+# CS 361: Project
+
+
+from google.cloud import datastore
+from json2html import *
+from urllib.parse import urlencode
+from flask import Flask, redirect, url_for, render_template, request, flash, session
+from helper_functions import calculate_article_health
+import flask
+import json
+import logging
+import requests
+import random
+import string
+
+
+# Creating flask app and generating secret key
+app = flask.Flask(__name__)
+app.secret_key = ''.join(random.sample(string.ascii_lowercase, 20))
+
+# Route to calculate article health and return working/non-working links
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == "POST":
+        
+        # Grabbing data
+        if request.content_type == "application/json":
+            content = request.get_json()
+            urls = content['links'].split(', ')
+        elif request.content_type in (
+            "application/x-www-form-urlencoded",
+            "multipart/form-data",
+        ):
+            urls = request.form['links'].split(', ')
+        else:
+            return (json.dumps({"Error":"Unsupported Media Type"}), 415)
+
+        if request.content_type == "application/json":
+            return (json.dumps(calculate_article_health(urls)))
+        else:
+            return render_template('links.html', articleLinks=calculate_article_health(urls))
+
+    else:
+        return ("Method not recognized")
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
